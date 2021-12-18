06f1ecf5db80dbe84e96771c20ca7609683b0ecd
Andrian Z <70290099+codingbeast666@users.noreply.github.com>
2021-12-17 21:44:06 -0500

    Updated it file check desc
    Previously, that was the local version.
    This code is the one that works for pythonanywhere.

--- WebService.py
+++ WebService.py
@@ -1,50 +1,57 @@
-import flask
+from flask import Flask
 from DAL.DBWeather import DBWeather
 from flask import request, jsonify
 from datetime import datetime
+from WebScraper import WeatherScraper
 import time
 
-app = flask.Flask(__name__)
+
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-system = DBWeather()
 
+#system = DBWeather()
+#scraper = WeatherScraper()
 
-@app.route("/pythonanywhere", methods=['GET'])
+@app.route("/", methods=['GET'])
 def select_all_temp():
+    system = DBWeather()
     temps = list(system.select_all_temp())
     return jsonify(temps)
 
 
-@app.route("/pythonanywhere/<int:temp_date>", methods=['GET'])
+@app.route("/<int:temp_date>", methods=['GET'])
 def select_temp(temp_date):
+    system = DBWeather()
     temp = system.select_all_temp(temp_date)
     return jsonify(temp)
 
 
-@app.route("/pythonanywhere", methods=['DELETE'])
+@app.route("/delete", methods=['DELETE'])
 def delete_all_temp():
+    system = DBWeather()
     temp = system.delete_all_temp()
     return jsonify(temp)
 
 
-@app.route("/pythonanywhere/new", methods=['POST'])
+@app.route("/", methods=['POST'])
 def post_temp():
-    # Receive data
-    data = request.json()
-    temp = data.get('temp')
+    system = DBWeather()
 
-    # Get data from webscraping
- externalService.getExternalTemp()
+    #weatherCom= scraper.get_current_temp_from_weathercom()
+    #meteoMedia= scraper.get_current_temp_from_meteomedia()
+
+    # Receive data
+    #data = request.json()
+    temp = request.json.get('temp')
+    weatherCom = request.json.get('tempWeatherCom')
+    meteoMedia = request.json.get('tempMeteoMedia')
 
     # Get Day, Month, Time
     current_date = datetime.now()
     t = time.localtime()
     current_time = time.strftime("%H:%M", t)
 
-    system.post_temp(12, 12, temp, current_date.day, current_date.month, current_time)
-    return 200
-
-
-app.run()
+    system.post_temp(weatherCom, meteoMedia, temp, current_date.day, current_date.month, current_time)
+    return "Posted Successfully"
